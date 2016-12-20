fb964f7da6ffde71b020f5ccce6dcab1c009d1c0
weizongwei5 <weizongwei5@gmail.com>
2016-12-20 20:45:28 +0800

    ip commit

--- autogetip_and_commit.py
+++ autogetip_and_commit.py
@@ -1,11 +1,7 @@
-
 #!/usr/bin/env python3
-# Note:
-#   Please save this as diagnostic_firstname.txt
-#       (replace firstname w/ yours)
-#   Do not edit lines that say: "# Last line in ____"
+# @weizongwei5
 ###############################################################################
-# Imports  # there will only be two imports added here.
+
 
 import socket
 import os
