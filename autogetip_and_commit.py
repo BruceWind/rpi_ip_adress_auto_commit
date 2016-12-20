a773bd56d78986817e4494b6c3e8daeffc438086
weizongwei5 <weizongwei5@gmail.com>
2016-12-20 20:40:08 +0800

    提交

--- autogetip_and_commit.py
+++ autogetip_and_commit.py
@@ -1,9 +1,22 @@
 import socket
+import os
+
+
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
-ip_adress = get_ip_address()
+ip_adress = get_ip_address() # current ip 
+cur_dir=os.getcwd() # current dir 
 
 print(ip_adress)
+os.chdir("/home/wei/git/rpi_ip_adress_auto_commit") # change dir
+
+fp = open('ip.txt', 'w') 
+fp.write(ip_adress) 
+
+
+
+
+
