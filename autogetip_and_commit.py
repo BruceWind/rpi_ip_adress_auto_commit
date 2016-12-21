88a2d11c9bd1905632158feeaaa1b951084cac47
weizongwei5 <weizongwei5@gmail.com>
2016-12-21 11:52:22 +0800

    ip commit on wei-All-Series

--- autogetip_and_commit.py
+++ autogetip_and_commit.py
@@ -34,31 +34,44 @@ i = 1
 while(i <= 10):
     is_valid=(internet_on())
     if(is_valid):
-        i=15
+        i=11
     time.sleep(3)
     i += 1
-
+# field
 username = getpass.getuser()
 separator = "/"
+
 if(is_valid):
+    print "start ping ....."
     ping_githost()
+    time.sleep(2)
+    print "get ip address ....."
     ip_adress = get_ip_address()  # current ip
+    time.sleep(2)
     cur_dir = os.getcwd()  # current dir
     git_dir = "/home/" + username + "/git"
 
     print("\t IP and Time:"+ip_adress+" "+ get_current_time()+"\n")
+    print "move dir   to gitdir ....."
     os.chdir(git_dir + separator + "rpi_ip_adress_auto_commit")  # change dir
+    time.sleep(2)
 
+    print "git pull"
     os.system('git pull')
+    time.sleep(2)
 
+    print "write ip to ip.txt"
     fp = open('ip.txt', 'w')
     fp.write(ip_adress +" "+ get_current_time()+"\n")
     fp.close()
+    time.sleep(2)
 
+    print "git commit  push"
     os.system('git add -A')
 
     os.system("git commit -m \'ip commit on " + socket.gethostname() + "\'")
 
+    time.sleep(2)
     os.system('git push')
 else:
     print('can\'t be  commit to github!\n\t无法提交到github!')
